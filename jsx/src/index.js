141a08da3bb3f10d047577db787b6bbeea3fc2c9
Pedro <pedrogusmao@ua.pt>
2020-02-07 15:53:29 +0000

    tests

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,10 +1,15 @@
+// Support IE11
+import 'react-app-polyfill/stable';
+import 'react-app-polyfill/ie11';
+
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>;
+    return <div>Hi there! Me nombre eres Pedro</div>;
 };
 
 // Take the react component and show it on the screen
