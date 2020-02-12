085f23f9d82852b55becda981c1b2f72bafe68b3
Pedro <pedrogusmao@ua.pt>
2020-02-12 15:49:51 +0000

    installing faker.js and new app project

--- components/src/index.js
+++ components/src/index.js
@@ -6,24 +6,28 @@ import 'react-app-polyfill/ie9';
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-/*
-function getButtonText() {
-    return "Click on me!";
-} */
-// Create a react component
+
 const App = () => {
-    const buttonText = { text: 'Click me!' }; 
-    const style = { backgroundColor: 'blue', color: 'white'}
     return (
-    <div>
-        <label htmlFor="name" className="label">Enter name:</label>
-        <input id="name"></input>
-        <button style={style}>
-            {buttonText.text}    
-        </button>
-    </div>    //jsx cant show javascript objects as they are not valid as react childs
-    );        //only properties!
-              //this example was made to mess around
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00 PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+        </div>
+
+
+    );        
 };
 
 // Take the react component and show it on the screen
