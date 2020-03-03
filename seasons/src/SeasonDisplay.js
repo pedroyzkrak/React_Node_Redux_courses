b59e7cdfe9f5c6829cb0da39b0670158c9bd15c2
Pedro Gusmão <pedroyzkrak@gmail.com>
2020-03-03 17:23:34 +0000

    class based components

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,16 @@
+import 'react-app-polyfill/stable';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/ie9';
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const SeasonDisplay = () => {
+    return <div>Season Display</div>;
+};
+
+
+export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,15 +2,33 @@ import 'react-app-polyfill/stable';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/ie9';
 
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const App = () => {
-    return <div>Hi there!</div>;
-};
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                //IMPORTANT
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => console.log(err)
+        );
+    }
+
+    // React requirement!
+    render() {
+        
+        return <div>Latitude: {this.state.lat}</div>;
+    }
+
+
+}
 
 
 ReactDOM.render(<App />, document.querySelector('#root'));
